34239f86953eef29bec855cb79e088e14f2bf6d6
Ryou <zhangliang@youlin.cn>
2016-08-25 18:43:43 +0800

    update code

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import tornado.web
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
-        self.write("Hello, world")
+        self.write("Hello, world from aws ecs")
 
 def make_app():
     return tornado.web.Application([
