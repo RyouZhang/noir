acedcb84b7102fab7cd80014b2c435cb0e8ad60c
Ryou <zhangliang@youlin.cn>
2016-09-09 12:14:25 +0800

    update code

--- service/__init__.py
+++ service/__init__.py
@@ -8,8 +8,8 @@ __all__ = (
 services = os.getenv('SERVICES', None)
 if services is not None:
     services = [s.strip() for s in services.split(',')]
-    valid_services = [service.replace('service.', '') for service in services if service.startswith('service.')]
+    valid_services = [service.replace('service.', '').split('.')[0] for service in services if service.startswith('service.')]
     if len(valid_services) > 0:
-        __all__ = [service.split('.')[0] for service in valid_services]
+        __all__ = valid_services
 
 from service import *
--- service/stock/__init__.py
+++ service/stock/__init__.py
@@ -9,6 +9,6 @@ if services is not None:
     services = [s.strip() for s in services.split(',')]
     valid_services = [service.replace('service.stock.', '') for service in services if service.startswith('service.stock.')]
     if len(valid_services) > 0:
-        __all__ = [service for service in valid_services]
+        __all__ = valid_services
 
 from service.stock import *
