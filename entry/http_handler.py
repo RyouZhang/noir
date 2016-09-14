507f8d7d65e6d949e26ab5d40b8651b7d1b36fc3
Ryou <zhangliang@youlin.cn>
2016-09-14 12:14:50 +0800

    add gzip deflate support

--- entry/http_handler.py
+++ entry/http_handler.py
@@ -55,13 +55,30 @@ class HttpRequestHandler(aiohttp.server.ServerHttpProtocol):
         response = aiohttp.Response(
             self.writer, 200, http_version = message.version
         )
+        response.add_header('Transfer-Encoding', 'chunked')
+
+        accept_encoding = message.headers.get('accept-encoding', '').lower()
+        if 'gzip' in accept_encoding:
+            response.add_header('Content-Encoding', 'gzip')
+            response.add_compression_filter('gzip')        
+        elif 'deflate' in accept_encoding:
+            response.add_header('Content-Encoding', 'deflate')
+            response.add_compression_filter('deflate')
+        
+        response.add_chunking_filter(1024)
+
+        output_raw = None
         if err is None:
-            response.add_header('HTTP_X_FIVEMILES_CODE', '500')
-        else:
             response.add_header('HTTP_X_FIVEMILES_CODE', '0')
-        response.send_headers()       
-        if err is None: 
-            response.write(json.dumps(raw).encode('utf-8'))
+            response.add_header('Content-type', 'text/json')
+            output_raw = json.dumps(raw).encode('utf-8')
         else:
-            response.write(err.encode('utf-8'))
+            response.add_header('HTTP_X_FIVEMILES_CODE', '500')
+            response.add_header('Content-type', 'text/plain')
+            output_raw = err.encode('utf-8')
+        response.send_headers()       
+
+        response.write(output_raw)
         await response.write_eof()
+        if response.keep_alive():
+            self.keep_alive(True)
