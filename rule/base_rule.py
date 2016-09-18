1bdb04eb82dfe9a898e7c108954a8fad17cb0638
Ryou <zhangliang@youlin.cn>
2016-09-18 14:58:40 +0800

    fix or rule bug

--- rule/base_rule.py
+++ rule/base_rule.py
@@ -16,6 +16,8 @@ def or_rule(config, params, context):
     rule_func_array = config.get('rules', None)
     if rule_func_array is None or len(rule_func_array) == 0:
         return True, None
+    
+    err = None
     for rule_func in rule_func_array:
         result, err = rule_func(params, context)
         if err is None:
