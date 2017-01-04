52bb12420334f12b8cd8040f016e354724ebfe74
Ryou <zhangliang@youlin.cn>
2017-01-04 12:15:17 +0800

    update code

--- entry/http_handler.py
+++ entry/http_handler.py
@@ -1,50 +1,34 @@
-import base64
 import time
-import json
 import asyncio
-from urllib.parse import urlparse, parse_qsl
-
 import aiohttp
 import aiohttp.server
-from aiohttp import MultiDict
 
 import router
 import service
 import util
 
 
+#async def parse_request_handler(message, payload) -> api, args, context
+#def prepare_response_handler(response, raw, err) -> reponse, raw
+
 class HttpRequestHandler(aiohttp.server.ServerHttpProtocol):
+    def __init__(self, parse_request_handler, prepare_response_handler, timeout=10):
+        self._parse_request_handler = parse_request_handler
+        self._prepare_response_handler = prepare_response_handler
+        self._timeout = timeout
+
 
     async def handle_request(self, message, payload):
         start_time = util.get_timestamp()
-        api, params, context = await self.parser_request(message, payload)
-        raw, err = await router.service_router.async_call_api(api, params, context, timeout=5)
-        util.logger.info('%s|%s|%s|%s', api, params, context, util.get_timestamp() - start_time)
-        await self.process_response(message, raw, err)
 
-    async def parser_request(self, message, payload):
-        url_info = urlparse(message.path)
-        path = url_info.path
+        if self._parse_request_handler is None:
+            await self.process_response(message, None, 'Invalid_Parse_Request')
+        else:
+            api, params, context = await self._parse_request_handler(message, payload)
+            raw, err = await router.service_router.async_call_api(api, params, context, self._timeout)
+            util.logger.info('%s|%s|%s|%s', api, params, context, util.get_timestamp() - start_time)
+            await self.process_response(message, raw, err)
 
-        args = {}
-        if message.method == 'GET':
-            pairs = parse_qsl(url_info.query)
-        elif message.method == 'POST':
-            raw = await payload.read()
-            pairs = parse_qsl(raw)
-        
-        for (k, v) in pairs:
-            args[k] = v
-        
-        context = dict(
-            sign=args.get('sign', None),
-            timestamp=args.get('e', 0),
-            user_id=message.headers.get('X_FIVEMILES_USER_ID', None),
-            user_token=message.headers.get('X_FIVEMILES_USER_TOKEN', None)
-        )
-        params = json.loads(args.get('args', '{}'))
-        
-        return path, params, context
 
     async def process_response(self, message, raw, err):
         try:
@@ -53,6 +37,9 @@ class HttpRequestHandler(aiohttp.server.ServerHttpProtocol):
                 self.writer, status, http_version = message.version, close = message.should_close
             )
             response.add_header('Transfer-Encoding', 'chunked')
+
+            if self._prepare_response_handler is not None:
+                response, raw= self._prepare_response_handler(response, raw, err)
 
             if err is None:
                 accept_encoding = message.headers.get('accept-encoding', '').lower()
@@ -65,15 +52,11 @@ class HttpRequestHandler(aiohttp.server.ServerHttpProtocol):
                     response.add_compression_filter('deflate')
                     response.add_chunking_filter(8192)
 
-                response.add_header('X_FIVEMILES_CODE', '0')
-                response.add_header('Content-type', 'application/json')
                 response.send_headers()
-
                 response.write(json.dumps(raw).encode('utf-8'))
             else:
-                response.add_header('X_FIVEMILES_CODE', '500')
-                response.add_header('X_FIVEMILES_MSG', base64.b64encode(err.encode('utf-8')).decode('utf-8'))
-                response.add_header('Content-type', 'text/plain')
+                if raw is not None:
+                    response.write(raw)
                 response.send_headers()
 
             await response.write_eof()
