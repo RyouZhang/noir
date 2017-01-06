946f676d5fad5d6793892845eecd18b98936e2f1
Ryou <zhangliang@youlin.cn>
2017-01-06 11:49:31 +0800

    update code

--- setup.py
+++ setup.py
@@ -6,11 +6,11 @@ from setuptools import setup
 setup(
     name='nori',
     version='0.0.1',
-    py_modules=['nori'],    
+    packages=['nori'],    
     description='fast web framework',
     author='ryou zhang',
     author_email='ryouzhang@gmail.com',
     install_requires=['uvloop', 'aiohttp', 'toml'],
     url='https://github.com/RyouZhang/nori/',
-    licence='Licence :: MTI Lecence'
+    licence='Licence :: MTI Lecence',
 )
