426f89372c608155bb139c4c7617539a1d514a2c
Ryou <zhangliang@youlin.cn>
2017-01-06 14:58:52 +0800

    fix bug

--- demo/service/loader.py
+++ demo/service/loader.py
@@ -5,7 +5,7 @@ def import_service(service_name):
     
     if services is not None:
         services = [s.strip() for s in services.split(',')]
-        valid_services = [service.replace(service_name, '').split('.')[1] for service in services if service.startswith(service_name)]
+        valid_services = [service.replace(service_name, '').split('.')[1] for service in services if service.startswith(service_name) and len(service.replace(service_name, '')) > 0]
         if len(valid_services) > 0:
             return valid_services
         return None
