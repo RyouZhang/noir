75b7e86bccbb81a2c5caa58132291b16745c3d34
Ryou <zhangliang@youlin.cn>
2017-01-06 11:32:36 +0800

    update setup

--- setup.py
+++ setup.py
@@ -1,13 +1,26 @@
-from distutils.core import setup
+import os
+import platform
+import sys
+import warnings
+
+try:
+    # Use setuptools if available, for install_requires (among other things).
+    import setuptools
+    from setuptools import setup
+except ImportError:
+    setuptools = None
+    from distutils.core import setup
+
+from distutils.core import Extension
 
 setup(
     name='nori',
     version='0.0.1',
+    packages=['nori'],    
     description='fast web framework',
     author='ryou zhang',
     author_email='ryouzhang@gmail.com',
     install_requires=['uvloop', 'aiohttp', 'toml'],
     url='https://github.com/RyouZhang/nori/',
-    packages=['nori'],
     licence='Licence :: MTI Lecence'
 )
