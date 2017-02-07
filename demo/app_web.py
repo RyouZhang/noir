cf250575fdc4c9a01b1782cd96099137d8b0f894
Ryou <zhangliang@youlin.cn>
2017-02-07 16:57:07 +0800

    demo support log level filter

--- demo/app_web.py
+++ demo/app_web.py
@@ -7,10 +7,8 @@ import logging
 import logging.config
 
 import noir.app as app
-# import noir.util.logging
 
 logging.config.dictConfig(toml.load(open('logging.toml')))
-# logging.getLogger().addFilter(noir.util.logging.filter.LogLevelFilter(min_level=logging.WARNING))
 
 server_port = os.getenv('SERVER_PORT', 8080)
 process_num = int(os.getenv('PROCESS_NUM', mp.cpu_count()))
