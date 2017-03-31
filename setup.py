47d826279bf4477fad895b6cba0d0eeafb74b63b
Ryou <zhangliang@youlin.cn>
2017-04-01 00:25:08 +0800

    update ver

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name='noir',
-    version='0.0.8',
+    version='0.0.10',
     description='A python fast web service base on aiohttp and uvloop, for python 3.5+',
     author='ryou zhang',
     author_email='ryouzhang@gmail.com',
